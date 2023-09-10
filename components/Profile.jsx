75e1675670944908a5d9592af146641aa49fac6b
luckystar20206 <luckystar20206@gmail.com>
2023-09-10 23:24:31 +0500

    id_profile title

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
     <section className="w-full">
       <h1 className="head_text text-left">
-        <span className="blue_gradient">{name} Profile</span>
+        <span className="blue_gradient">{`${name}'s Profile`}</span>
       </h1>
       <p className="desc text-left">{desc}</p>
 
